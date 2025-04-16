f3b453e23b21e34d3b95df8063adbfde3f65135f
Dade <hackerdade@gmail.com>
2025-04-16 11:00:58 +0800

    move getCloudflareContext

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,14 +2,14 @@ import { createOpenAI } from "@ai-sdk/openai"
 import { getCloudflareContext } from "@opennextjs/cloudflare"
 import { Message, streamText } from "ai"
 
-const { env } = getCloudflareContext()
-
-const openai = createOpenAI({
-  apiKey: env.OPENAI_API_KEY,
-  baseURL: `https://gateway.ai.cloudflare.com/v1/c5b793f894444f1cc7aaac929176106d/dade-ca/openai`,
-})
-
 export async function POST(req: Request) {
+  const { env } = getCloudflareContext()
+
+  const openai = createOpenAI({
+    apiKey: env.OPENAI_API_KEY,
+    baseURL: `https://gateway.ai.cloudflare.com/v1/c5b793f894444f1cc7aaac929176106d/dade-ca/openai`,
+  })
+
   const { messages } = (await req.json()) as { messages: Message[] }
 
   const result = streamText({
