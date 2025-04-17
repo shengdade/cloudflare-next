be2ad2d7bc746dd72ce55a4e4868d3e24f68f46f
Dade <hackerdade@gmail.com>
2025-04-17 11:29:36 +0800

    added build var and now remove re_123

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,6 +1,6 @@
 import { CreateEmailOptions, Resend } from "resend"
 
-const resend = new Resend(process.env.RESEND_API_KEY ?? "re_123")
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendEmail(payload: CreateEmailOptions) {
   const { data, error } = await resend.emails.send(payload)
